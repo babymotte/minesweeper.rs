4ac1df3ef2929ebc1cadc4d54f0b3b401e8d66ed
Michael Bachmann <kontakt@babymotte.de>
2017-03-13 17:51:02 +0100

    some more refactoring

--- src/core.rs
+++ src/core.rs
@@ -64,7 +64,7 @@ impl MineField {
             height: game_parameters.1,
         };
 
-        mf.fill(game_parameters.2, blank_x, blank_y);
+        fill(&mut mf, game_parameters.2, blank_x, blank_y);
 
         mf
     }
@@ -82,12 +82,16 @@ impl MineField {
     }
 
     pub fn get_mine_count(&self) -> usize {
-        self.tiles.iter().fold(0, |i, t| if t.mine {i + 1} else {i})
+        self.tiles.iter().fold(0, |i, t| if t.mine { i + 1 } else { i })
     }
 
     pub fn get_unmarked_mine_count(&self) -> usize {
         let mine_count = self.get_mine_count();
-        let flag_count = self.tiles.iter().fold(0, |i, t| if t.state == TileState::Marked {i + 1} else {i});
+        let flag_count = self.tiles.iter().fold(0, |i, t| if t.state == TileState::Marked {
+            i + 1
+        } else {
+            i
+        });
         mine_count - flag_count
     }
 
@@ -122,7 +126,7 @@ impl MineField {
     }
 
     pub fn get_nearby_coordinates(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
-        let nearby_indices = self.get_nearby_indices(x as i8, y as i8);
+        let nearby_indices = get_nearby_indices(self, x as i8, y as i8);
         nearby_indices.iter()
             .map(|i| (i % self.get_width() as usize, i / self.get_width() as usize))
             .collect()
@@ -131,42 +135,6 @@ impl MineField {
     fn get_mut_tile(&mut self, x: usize, y: usize) -> &mut Tile {
         let i = self.to_index(x, y);
         &mut self.tiles[i]
-    }
-
-    fn fill(&mut self, mines: usize, blank_x: usize, blank_y: usize) {
-
-        let width = self.width;
-        let height = self.height;
-
-        let mine_coordinates = generate_mine_coordinates(width, height, mines, blank_x, blank_y);
-
-        create_tiles(&mut self.tiles, width, height, mine_coordinates);
-
-        self.update_nearby_mine_counts();
-    }
-
-    fn update_nearby_mine_counts(&mut self) {
-
-        for i in 0..self.tiles.len() {
-            self.tiles[i].nearby_mines = self.count_nearby_mines(&self.tiles[i]);
-        }
-    }
-
-    fn count_nearby_mines(&self, tile: &Tile) -> u8 {
-
-        let is = self.get_nearby_indices(tile.x as i8, tile.y as i8);
-        let mines: Vec<usize> = is.into_iter().filter(|i| self.tiles[*i].mine).collect();
-
-        mines.len() as u8
-    }
-
-    fn get_nearby_indices(&self, x: i8, y: i8) -> Vec<usize> {
-        let os: [(i8, i8); 8] = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1),
-                                 (1, 1)];
-        let cs = os.iter().map(|o| ((x + o.0) as usize, (y + o.1) as usize));
-        let filtered_cs = cs.filter(|c| c.0 < self.width && c.1 < self.height);
-        let is = filtered_cs.map(|c| self.to_index(c.0, c.1));
-        is.collect()
     }
 
     fn to_index(&self, x: usize, y: usize) -> usize {
@@ -182,6 +150,42 @@ pub fn get_params_for_difficulty(level: Difficulty) -> (usize, usize, usize) {
         Difficulty::Expert => (30, 16, 99),
         Difficulty::Custom(w, h, m) => (w, h, m),
     }
+}
+
+fn fill(mf: &mut MineField, mines: usize, blank_x: usize, blank_y: usize) {
+
+    let width = mf.width;
+    let height = mf.height;
+
+    let mine_coordinates = generate_mine_coordinates(width, height, mines, blank_x, blank_y);
+
+    create_tiles(&mut mf.tiles, width, height, mine_coordinates);
+
+    update_nearby_mine_counts(mf);
+}
+
+fn update_nearby_mine_counts(mf: &mut MineField) {
+
+    for i in 0..mf.tiles.len() {
+        mf.tiles[i].nearby_mines = count_nearby_mines(mf, &mf.tiles[i]);
+    }
+}
+
+fn count_nearby_mines(mf: &MineField, tile: &Tile) -> u8 {
+
+    let is = get_nearby_indices(mf, tile.x as i8, tile.y as i8);
+    let mines: Vec<usize> = is.into_iter().filter(|i| mf.tiles[*i].mine).collect();
+
+    mines.len() as u8
+}
+
+
+fn get_nearby_indices(mf: &MineField, x: i8, y: i8) -> Vec<usize> {
+    let os: [(i8, i8); 8] = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)];
+    let cs = os.iter().map(|o| ((x + o.0) as usize, (y + o.1) as usize));
+    let filtered_cs = cs.filter(|c| c.0 < mf.width && c.1 < mf.height);
+    let is = filtered_cs.map(|c| mf.to_index(c.0, c.1));
+    is.collect()
 }
 
 fn generate_mine_coordinates(width: usize,
@@ -210,12 +214,12 @@ fn create_tiles(tiles: &mut Vec<Tile>,
 fn create_tile(tiles: &mut Vec<Tile>, x: usize, y: usize, mine_coordinates: &Vec<(usize, usize)>) {
     let mine = mine_coordinates.contains(&(x, y));
     tiles.push(Tile {
-        state: TileState::Covered,
-        x: x,
-        y: y,
-        mine: mine,
-        nearby_mines: 0,
-    });
+                   state: TileState::Covered,
+                   x: x,
+                   y: y,
+                   mine: mine,
+                   nearby_mines: 0,
+               });
 }
 
 fn is_clear(tiles: &Vec<Tile>) -> bool {
