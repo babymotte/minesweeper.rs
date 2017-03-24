6e589b174d42979082e4d5dd64529b195105fda6
Michael Bachmann <kontakt@babymotte.de>
2017-03-24 19:18:58 +0100

    added native hooks

--- src/minesweeper_u32.rs
+++ src/minesweeper_u32.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 extern crate regex;
+extern crate libc;
 
 use std::io;
 use std::time::Duration;
@@ -8,16 +9,22 @@ use interface::{GameHandle, GameState};
 use highscores::Highscores;
 use highscores;
 use regex::Regex;
+use libc::c_uint;
 
-pub fn start(level: Difficulty) {}
-pub fn uncover_tile(x: usize, y: usize) {}
-pub fn toggle_flag(x: usize, y: usize) {}
+#[no_mangle]
+pub extern fn command(name: *const c_uint) {}
+
+#[no_mangle]
+pub extern fn hook(callback: extern "C" fn(*const c_uint)) {}
+
+fn start(level: Difficulty) {}
+fn uncover_tile(x: usize, y: usize) {}
+fn toggle_flag(x: usize, y: usize) {}
 
 fn report_game_state_change(state: GameState) {}
 fn report_tile_state_change(state: TileState) {}
 fn report_time_change(state: Duration) {}
 fn report_new_highscore(state: Duration) {}
-
 
 fn convert_game_state_change(state: GameState) -> u32 {0}
 fn convert_tile_state_change(state: TileState) -> u32 {0}
